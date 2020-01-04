f6276884674a004bdb4acdeac1f2c54549704d49
lobo <lobo@localhost.localdomain>
2020-01-04 16:14:55 -0300

    Adding keys to access the default Twitter API

---
+++ src/keys.py
@@ -0,0 +1,6 @@
+# My personal Twitter API keys
+# see website https://developer.twitter.com/
+consumer_key = 'useYourKey'
+consumer_secret = 'useYourKey'
+access_token = 'useYourKey'
+access_token_secret = 'useYourKey'

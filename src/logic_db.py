08559c557e86b47cb40b6f7ecc478ae349f96c7d
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-08 11:39:13 -0200

    maturing bson to json conversion logic

--- src/logic_db.py
+++ src/logic_db.py
@@ -1,35 +1,32 @@
 from pymongo import MongoClient
 from mongo_config import *
 from bson.objectid import ObjectId
-from bson.json_util import dumps
+from bson.json_util import dumps, loads, CANONICAL_JSON_OPTIONS
 import json 
 
 # Access collection into twitterdb
 collection = db['twitter_search']
 
-
-
 # The five users with the most followers
 find_fl = collection.find({}, {"followers_count": 1, "user_name": 1}).limit(5).sort("followers_count", -1)
 
-
 # mongodata into list
-#five_most_list = list(find_fl)
+five_most_list = list(find_fl)
 
-"""
 # define encode and convert BSON to JSON
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
         return json.JSONEncoder.default(self, o)
-
 five_most_followers = JSONEncoder().encode(five_most_list)
 
-x = dumps(five_most_followers)
-print(x)
+json_format_fl = json.loads(five_most_followers)
 
-"""
+
+#print(five_most_followers["_id"])
+
+
 
 
 # Total posts grouped by time of day

fb863b479aafdead39a6a4319606bd2630ef0e23
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-09 15:54:49 -0200

    adding oo structure

--- src/collect_data.py
+++ src/collect_data.py
@@ -4,50 +4,55 @@ import json
 from oauth import *
 from mongo_config import *
 
-# Function for collect tweets
-def get_tweets(hashtags): 
-          
-        # Number of tweets 
-        number_of_tweets=100
+class CollectData():
+    
+    def __init__(self, number_of_tweets, last_id, hashtags):
+        
+        # Number of tweets
+        self.number_of_tweets = number_of_tweets
+        # Latests ids
+        self.last_id = last_id
+        # Hashtags
+        self.hashtags = hashtags
+        
+    # Function for collect tweets
+    def get_tweets(self): 
+            
+            # Search only new tweets with api.search
+            api_search = api.search(q=self.hashtags, count=self.number_of_tweets, show_user=True, include_rts=False,tweet_mode='extended', include_entities=True, max_id=str(self.last_id), since='2019-01-01')
 
-        # latest ids
-        last_id = -1
+            for info in api_search:
 
-        # Search only new tweets with api.search
-        api_search = api.search(q=hashtags, count=number_of_tweets, show_user=True, include_rts=False,tweet_mode='extended', include_entities=True, max_id=str(last_id - 1), since='2019-01-01')
+                if info.full_text.startswith("RT @") == True:
+                    pass
+                else:
+                    dict_ = {'hashtag': self.hashtags,
+                            'screen_name': info.user.screen_name,
+                            'user_name': info.user.name,
+                            'tweet': info.full_text,
+                            'tweet_created_at': (str(info.created_at)),
+                            'language': info.metadata['iso_language_code'],
+                            'followers_count': info.user.followers_count
+                    
+                    }
 
-        for info in api_search:
+                    # Convert dict_ in json 
+                    mongo_data_json = json.dumps(dict_, ensure_ascii=False)
 
-            if info.full_text.startswith("RT @") == True:
-                pass
-            else:
-                dict_ = {'hashtag': hashtags,
-                        'screen_name': info.user.screen_name,
-                        'user_name': info.user.name,
-                        'tweet': info.full_text,
-                        'tweet_created_at': (str(info.created_at)),
-                        'language': info.metadata['iso_language_code'],
-                        'followers_count': info.user.followers_count
-                
-                }
+                    # Decode the JSON from Twitter
+                    datajson = json.loads(mongo_data_json)
 
-                # Convert dict_ in json 
-                mongo_data_json = json.dumps(dict_, ensure_ascii=False)
-
-                # Decode the JSON from Twitter
-                datajson = json.loads(mongo_data_json)
-
-                # Insert json into mongo
-                db.twitter_search.insert_one(datajson)
-  
+                    # Insert json into mongo
+                    db.twitter_search.insert_one(datajson)
+    
 # Call code
 if __name__ == '__main__': 
-  
-
+    
     # collect data 
     tags=['#openbanking','#apifirst','#devops','#cloudfirst','#microservices','#apigateway','#oauth', '#swagger','#raml','#openapis']
-    for x in tags:
-        get_tweets(x)  
+    for hashtags in tags:
+         pdatabase = CollectData(100, -1, hashtags)
+         pdatabase.get_tweets()  
 
 # Test post one tweet 
 #api.update_status("OI!")

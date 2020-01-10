cf58126a70146d1cecf4319cddbeab68e6934a00
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-10 16:33:33 -0300

    add news routes

--- src/rest_api.py
+++ src/rest_api.py
@@ -3,15 +3,26 @@
 import json
 from flask import Flask, jsonify
 from src import logic_db
+from flask_cors import CORS, cross_origin
 
 # Prometheus exporter lib
 from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-tasks = logic_db.json_format_fl
 
-@app.route('/api', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+@app.route('/api/show-fls', methods=['GET'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def get_find_fl():
+    return jsonify(logic_db.json_format_fl)
+
+@app.route('/api/group-hrs', methods=['GET'])
+def get_group_by_hr():
+    return jsonify(logic_db.group_list_by_hours)
+
+
+@app.route('/api/group-lang', methods=['GET'])
+def get_group_by_lang():
+    return jsonify(logic_db.group_list_by_flag)
 

4c4483f757e35f24da132940fa440a439ad1dbfe
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-10 22:11:48 -0300

    These keys belong to a temporary account, no risks.

--- src/keys.py
+++ src/keys.py
@@ -1,6 +1,6 @@
 # My personal Twitter API keys
 # see website https://developer.twitter.com/
-consumer_key = 'useYourKey'
-consumer_secret = 'useYourKey'
-access_token = 'useYourKey'
-access_token_secret = 'useYourKey'
+consumer_key = 'XiHRGJH9nJepnhNnYilkwg45i'
+consumer_secret = 'OblR3hur3RdslpvCyfPC0wYQ4pdU80enW7apQtOf3ibCT87O9M'
+access_token = '1157999477984243713-WqcwdvsbgJHyeD6v0sULRFaOvuDxI2'
+access_token_secret = 'ikMsuLMdxm0UECu5JmlPPICK6nc3mlpAH4KRBtnQMGHne'

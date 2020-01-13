8ed0f297d987ef787220d6210064f79b3f5b5897
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-13 18:00:00 -0200

    add new querie

--- twittercollector/rest_api.py
+++ twittercollector/rest_api.py
@@ -10,15 +10,19 @@ app = Flask(__name__)
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+
+# Route from question a
 @app.route('/api/show-fls', methods=['GET'])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_find_fl():
     return jsonify(json_format_fl)
 
+# Route from question b
 @app.route('/api/group-hrs', methods=['GET'])
 def get_group_by_hr():
     return jsonify(aggr_by_hr)
 
+# Route from question c
 #@app.route('/api/group-lang', methods=['GET'])
 #def get_group_by_lang():
 #    return jsonify(logic_db.group_lang)

8b0638a1eb2c844a47642074c297f8efcc9e5d1b
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-14 15:53:18 -0200

    adding check if collection exist

--- twittercollector/mongo_config.py
+++ twittercollector/mongo_config.py
@@ -5,6 +5,14 @@ mongo_host= ('mongodb://localhost/twitterdb')
 
 client = MongoClient(mongo_host)
 
-# Use twitterdb database. If it doesn't exist, it will be created.
-db = client.twitterdb
+# check if collection already exist
+appdb = client['twitterdb']
+app_collection = appdb['twitter_search']
+
+collection_name = 'twitter_search'
+if collection_name in appdb.list_collection_names():
+    print('\nThis collection already exists in db.\n')
+else:
+    # Use twitterdb database. If it doesn't exist, it will be created.
+    db = client.twitterdb
 

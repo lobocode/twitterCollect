74bf797cad1e4a89d812168de79b18de6ec13461
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-14 11:40:10 -0200

    improving the name of some variables

--- twittercollector/logic_db.py
+++ twittercollector/logic_db.py
@@ -13,19 +13,19 @@ This module contains all queries and logic applied to the database.
 collection = db['twitter_search']
 
 # pymongo query to mongodb
-find_fl = collection.aggregate([
+q_aggr_fl = collection.aggregate([
     {'$group': {'_id': {"user_name": "$user_name", "followers_count": "$followers_count"}}},
     {'$sort': {"_id.followers_count": -1}},
     {'$limit': 5}
 ])
 
 # json_format_fl var convert cursor query in list, serialization bson, convert bson in json and deserialize
-json_format_fl = json.loads(dumps(list(find_fl)))
+list_aggr_fl = json.loads(dumps(list(q_aggr_fl)))
 
 # b question #######################################################################################
 
 # pymongo query to mongodb
-find_aggregate_by_hour = collection.aggregate([
+q_aggr_by_hour = collection.aggregate([
   {
     "$addFields": {
       "tweet_created_at": {
@@ -56,8 +56,9 @@ find_aggregate_by_hour = collection.aggregate([
   }
 ])
 
+
 # json_format_fl var convert cursor query in list, serialization bson, convert bson in json and deserialize
-aggr_by_hr = json.loads(dumps(list(find_aggregate_by_hour)))
+list_aggr_by_hr = json.loads(dumps(list(q_aggr_by_hour)))
 
 
 # c question #######################################################################################

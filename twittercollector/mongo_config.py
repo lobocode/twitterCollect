9e9e87950d07afc52b24d86e18af992c8c561d94
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-14 23:12:04 -0300

    fixing some bugs in the call

--- twittercollector/mongo_config.py
+++ twittercollector/mongo_config.py
@@ -6,13 +6,7 @@ mongo_host= ('mongodb://localhost/twitterdb')
 client = MongoClient(mongo_host)
 
 # check if collection already exist
-appdb = client['twitterdb']
-app_collection = appdb['twitter_search']
 
-collection_name = 'twitter_search'
-if collection_name in appdb.list_collection_names():
-    print('\nThis collection already exists in db.\n')
-else:
-    # Use twitterdb database. If it doesn't exist, it will be created.
-    db = client.twitterdb
+# Use twitterdb database. If it doesn't exist, it will be created.
+db = client.twitterdb
 

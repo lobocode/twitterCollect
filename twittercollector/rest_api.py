78fc27cd1fa0a9e83d03d3532181209878d563c9
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-15 16:47:16 -0200

    defining a better implementation for logs with flask_graylog

--- twittercollector/rest_api.py
+++ twittercollector/rest_api.py
@@ -6,48 +6,54 @@ from flask import Flask, jsonify
 from logic_db import *
 from mongo_config import *
 from flask_cors import CORS, cross_origin
+from flask_graylog import Graylog
 
-# Get logger with graylog handler
-log = logging.getLogger()
-handler = graypy.GELFUDPHandler('localhost', 12201)
-log.addHandler(handler)
 
 app = Flask(__name__)
+graylog = Graylog(app)
+
+# Get logger with graylog handler
+handler = graypy.GELFUDPHandler('localhost', 12201)
+logger = logging.getLogger(__name__)
+logger.addHandler(graylog.handler)
+
+# Log to graylog
+graylog.info('Message', extra={
+    'extra': 'metadata',
+})
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Route from question a
 @app.route('/api/show-fls', methods=['GET'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def get_find_fl():
-    log.info('info')
+    logger.info('info')
     return jsonify(list_aggr_fl)
 
 # Route from question b
 @app.route('/api/group-hrs', methods=['GET'])
 def get_group_by_hr():
-    #log.info('testando aggr by hour')
-    #app.logger.info('info')
+    logger.info('testando aggr by hour')
+    # app.logger.info('info')
     return jsonify(list_aggr_by_hr)
 
 
 # Route from question c
-#@app.route('/api/group-lang', methods=['GET'])
-#def get_group_by_lang():
+# @app.route('/api/group-lang', methods=['GET'])
+# def get_group_by_lang():
 #    return jsonify(logic_db.group_lang)
 
 
-# This route doesn't exist. 
+# This route doesn't exist.
 # This function purposely calls an error (error 500).
 @app.route('/api/error', methods=['GET'])
 def get_error():
-    return error # Doesn't exist this var
-
-
-
+    logger.error('An occurred error')
+    return error  # Doesn't exist this var
 
 
 if __name__ == '__main__':
-    
+
     # run REST API
     app.run()

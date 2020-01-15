846a06528db93cec0c9156df1fa43ec68ce3c78d
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-15 05:57:34 -0300

    better adapting the logging system

--- twittercollector/rest_api.py
+++ twittercollector/rest_api.py
@@ -3,35 +3,42 @@ import graypy
 import logging
 import json
 from flask import Flask, jsonify
-#from flask import current_app, Flask, jsonify
 from logic_db import *
 from mongo_config import *
 from flask_cors import CORS, cross_origin
+
+# Get logger
+def buildLogging():
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.INFO)
+    handler = graypy.GELFUDPHandler('localhost', 12201)
+    log.addHandler(handler)
 
 app = Flask(__name__)
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+buildLogging()
+
 # Route from question a
 @app.route('/api/show-fls', methods=['GET'])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def get_find_fl():
-    app.logger.info('Info')
+    app.logger.info('info')
     return jsonify(list_aggr_fl)
 
 # Route from question b
 @app.route('/api/group-hrs', methods=['GET'])
 def get_group_by_hr():
-    app.logger.info('Info')
+    app.logger.info('info')
     return jsonify(list_aggr_by_hr)
-
-    
 
 
 # Route from question c
 #@app.route('/api/group-lang', methods=['GET'])
 #def get_group_by_lang():
 #    return jsonify(logic_db.group_lang)
+
 
 # This route doesn't exist. 
 # This function purposely calls an error (error 500).
@@ -46,10 +53,5 @@ def get_error():
 
 if __name__ == '__main__':
     
-    # Call Graylog handler connect
-    handler = graypy.GELFHTTPHandler('localhost', 12201)
-    handler.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
-     
     # run REST API
     app.run()

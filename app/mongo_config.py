e9c5a4790fcf943268ec886f22af05c0143564c6
Vitor Lobo Ramos <lobocode@gmail.com>
2020-01-20 16:08:56 -0200

    fix mongoconfig

--- app/mongo_config.py
+++ app/mongo_config.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # Assuming you have mongoDB installed locally
-mongo_host= ('mongodb://mongodb/twitterdb')
+mongo_host= ('mongodb://mongo/twitterdb')
 #mongo_host= ('mongodb://localhost/twitterdb')
 
 client = MongoClient(mongo_host)
